c4c52e2f1e0f7dac22b9d6b9c028d6173b72f3f9
mwpas <mwpas@MIKEPC>
2025-07-31 22:17:03 -0400

    more texture alignment

--- WadMaker.Tests/Services/TextureAdjusterTests.cs
+++ WadMaker.Tests/Services/TextureAdjusterTests.cs
@@ -20,6 +20,29 @@ internal class TextureAdjusterTests : StandardTest
         Assert.That(elements.LineDefs[1].Front.Data.offsetx, Is.EqualTo(6));
         Assert.That(elements.LineDefs[2].Front.Data.offsetx, Is.EqualTo(42));
         Assert.That(elements.LineDefs[3].Front.Data.offsetx, Is.EqualTo(48));
+    }
 
+    [Test]
+    public void CanAlignMultipleTextures()
+    {
+        var map = new Map();
+        map.Rooms.Add(new Room
+        {
+            UpperLeft = Point.Empty,
+            BottomRight = new Point(70, -100),
+        });
+        map.Rooms[0].SideTextures[Side.Left] = new TextureInfo(Texture.BRICK7);
+        map.Rooms[0].SideTextures[Side.Top] = new TextureInfo(Texture.BRICK7);
+        map.Rooms[0].SideTextures[Side.Right] = new TextureInfo(Texture.BRICK6);
+        map.Rooms[0].SideTextures[Side.Bottom] = new TextureInfo(Texture.BRICK6);
+
+
+        var elements = MapBuilder.Build(map);
+        TextureAdjuster.AdjustOffsetsAndPegs(elements);
+
+        Assert.That(elements.LineDefs[0].Front.Data.offsetx, Is.EqualTo(36)); // top
+        Assert.That(elements.LineDefs[1].Front.Data.offsetx, Is.EqualTo(0)); // right
+        Assert.That(elements.LineDefs[2].Front.Data.offsetx, Is.EqualTo(36)); // bottom
+        Assert.That(elements.LineDefs[3].Front.Data.offsetx, Is.EqualTo(0)); // left
     }
 }
--- WadMaker/Services/TextureAdjuster.cs
+++ WadMaker/Services/TextureAdjuster.cs
@@ -8,15 +8,72 @@ public class TextureAdjuster
     /// <param name="mapElements"></param>
     public void AdjustOffsetsAndPegs(MapElements mapElements)
     {
-        int totalWallWidth = 0;
-        
-        foreach(var line in LineWithNeighbors(mapElements.LineDefs.First(), mapElements))
+        foreach(var textureRun in TextureRuns(mapElements))
         {
-            var textureSize = DoomTextureConfig.DoomTextureSizes[line.Front.Data.texturemiddle.ToString()];
+            AlignTextures(textureRun);
+        }
+    }
+
+    private void AlignTextures(LineDef[] lines)
+    {
+        int totalWallWidth = 0;
+
+        foreach (var line in lines)
+        {
+            var textureSize = DoomTextureConfig.DoomTextureSizes[line.Front.Data.texturemiddle];
             line.Front.Data = line.Front.Data with { offsetx = totalWallWidth % textureSize.Width };
             totalWallWidth += (int)line.Length;
         }
     }
+
+    /// <summary>
+    /// Gets all sequences of linedefs that share the same texture
+    /// </summary>
+    /// <param name="mapElements"></param>
+    /// <returns></returns>
+    private IEnumerable<LineDef[]> TextureRuns(MapElements mapElements)
+    {
+        var linesWithNeighbors = mapElements.LineDefs
+            .Select(p => LineWithNeighbors(p, mapElements).ToArray())
+            .GroupBy(p => String.Join(",", p.Select(q => mapElements.LineDefs.IndexOf(q)).Order().ToArray()))
+            .Select(p => p.First())
+            .ToArray();
+
+        return linesWithNeighbors.SelectMany(p => TextureRuns(p));
+    }
+
+    private IEnumerable<LineDef[]> TextureRuns(LineDef[] run)
+    {
+        var lastTexture = run[0].Front.Data.texturemiddle;
+        List<LineDef[]> runs = new List<LineDef[]>();
+        List<LineDef> currentRun = new List<LineDef>();
+
+        foreach(var line in run)
+        {
+            if (line.Front.Data.texturemiddle != lastTexture)
+            {
+                runs.Add(currentRun.ToArray());
+                currentRun.Clear();
+                lastTexture = line.Front.Data.texturemiddle;
+            }
+            
+            currentRun.Add(line);            
+        }
+
+        if (runs.Count() > 0 && lastTexture == run.First().Front.Data.texturemiddle)
+        {
+            currentRun.AddRange(runs.First());
+            runs[0] = currentRun.ToArray();
+        }
+        else
+        {
+
+            runs.Add(currentRun.ToArray());
+        }
+
+        return runs;
+    }
+
 
     private IEnumerable<LineDef> LineWithNeighbors(LineDef line, MapElements mapElements)
     {
